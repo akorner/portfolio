c84e3742a1540544d1f6a9ff9537187b7dd27cac
Ari Korner <arikorner@Aris-MacBook-Pro.local>
2015-08-06 20:46:58 -0400

    added js
    js for clicking on image and taking you to new page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function() {
   // Handler for .ready() called.
+
+
+  //anything with class hotzone, when clicked, will load http:// contained in "data-link"
+
+  $('.hotzone').each(function(){
+  	$(this).click(function(e){
+  		e.preventDefault();
+  		var link = $(this).attr('data-link');
+  		//alert(link);
+  		window.location.href = link;
+  	});
+  });
 });
